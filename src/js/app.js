1776c2125a74442497f922099706387627891b92
Юлия Михайлова <jullidu@gmail.com>
2020-01-23 15:53:26 +0800

    finish project

--- src/js/app.js
+++ src/js/app.js
@@ -30,3 +30,41 @@ for (let i = 0; i < data.checkbox.length; i++) {
 
 }
 elem.innerHTML = htmlStr
+
+//setting slider value
+
+let myValue = document.querySelector('#myRange')
+myValue.value = data.level
+
+
+//slider value for mobile
+
+function setSliderSkillLevel(number) {
+  if (number < 20) {
+    level.innerHTML = 'Не владею'
+  } else if (number < 50) {
+    level.innerHTML = 'Использую готовые решения'
+  } else if (number < 80) {
+    level.innerHTML = 'Использую готовые решения и умею их переделывать'
+  } else {
+    level.innerHTML = 'Пишу сложный JS с нуля'
+  }
+}
+
+let level = document.querySelector('#slider-skill-level')
+let myScore = myValue.value
+
+setSliderSkillLevel(myScore)
+
+
+myValue.addEventListener('change', (event) => {
+  let result = event.target.value
+  setSliderSkillLevel(result)
+})
+
+
+
+//rendering date
+
+let date = document.querySelector('#date')
+date.innerHTML = data.date
